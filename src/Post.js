2f51b4aeb0490c502d6384e96eed7d8139ab20d4
DinhSonHai <sdinhhai@gmail.com>
2020-10-03 23:36:00 +0700

    Add firebase-tools and create Post component

---
+++ src/Post.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Post = () => {
+  return (
+    <div className='post'>
+      {/* header -> avatar + username */}
+
+      {/* image */}
+
+      {/* username + caption */}
+    </div>
+  );
+};
+
+export default Post;
